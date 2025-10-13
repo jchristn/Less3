6a7849098ae621b02ddbf18c952b919a2f2e6fa7
Joel Christner <joel.christner@gmail.com>
2025-10-13 13:48:25 -0700

    NuGet v2.1.12, dep update, docker improvements

---
+++ src/build-docker.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+IF "%1" == "" GOTO :Usage
+ECHO.
+ECHO Building for linux/amd64 and linux/arm64/v8...
+docker buildx build -f Less3\Dockerfile --builder cloud-viewio-assistant-builder --platform linux/amd64,linux/arm64/v8 --tag jchristn/less3:%1 --push .
+GOTO :Done
+
+:Usage
+ECHO.
+ECHO Provide an argument specifying the version or tag.
+ECHO Example: dockerbuild.bat v2.1.12
+
+:Done
+ECHO.
+ECHO Done
+@ECHO ON

b868a5d55a1fc1bd499f51e02ebefac8062d8539
Joel Christner <joel.christner@gmail.com>
2026-02-20 14:48:35 -0800

    Docker LESS3_SERVER_URL

--- dashboard/build-dashboard.bat
+++ dashboard/build-dashboard.bat
@@ -2,7 +2,7 @@
 IF "%1" == "" GOTO :Usage
 ECHO.
 ECHO Building for linux/amd64 and linux/arm64/v8...
-docker buildx build -f Dockerfile --builder cloud-viewio-assistant-builder --platform linux/amd64,linux/arm64/v8 --tag jchristn/less3-ui:%1 --push .
+docker buildx build -f Dockerfile --builder cloud-jchristn77-jchristn77 --platform linux/amd64,linux/arm64/v8 --tag jchristn77/less3-ui:%1 --push .
 GOTO :Done
 
 :Usage
